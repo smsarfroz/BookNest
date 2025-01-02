96e71eb118b71b7290be2c4e745e5f643affef30
Sheikh Sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-01-02 13:23:37 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = 5555;
+export const PORT = process.env.PORT || 5555;
 
 export const mongoDBURL =
     'mongodb+srv://ssarfroz:smBUKnBfbz17VVGx@bookstore-mern.xcolc.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore-MERN'
