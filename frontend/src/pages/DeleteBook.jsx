58622be2bccf3dd8b322adbdcd4b7c9d0b49a0be
Sheikh Sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-01-02 12:13:34 +0530

    Update DeleteBook.jsx

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`http://localhost:5555/books/${id}`)
+      .delete(`https://book-nest-mwi9kfq1z-sarfrozs-projects.vercel.app/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book deleted successfully', { variant: 'success'});
